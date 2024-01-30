d2a108f802aa8e96118fd0f43c139c8925858ae2
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-01-30 20:12:28 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,18 +3,12 @@ let numeroMaximo = 5000
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute
 let tentativas = 1;
-//ambiente secreto dos DEV's
 console.log('númerosecreto:', numeroSecreto); 
 console.log('Valor do chute:', chute);
-//a vatiável chute foi criada para armazenar o número digitado no prompt
 
-// Operadores: sinais de comparação: Igual == ; Não é igual != ; Maior > ;
-
-// enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`); 
     
-    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
     }else { 
@@ -23,7 +17,6 @@ while (chute != numeroSecreto) {
         } else {
             alert('O número secreto é maior que ' + chute );
         }
-        //tentativas = tentativas + 1;
         tentativas++
     }
 }
