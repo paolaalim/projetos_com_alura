7cf7b04607ca1be1b75b35ca7676e81e2b62cae8
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-01-31 12:08:50 -0300

    Add files via upload

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,52 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirtextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
+}
+function mensagemInicial(params) {
+    exibirtextoNaTela('h1', 'Jogo do número secreto');
+    exibirtextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+mensagemInicial();
+
+
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirtextoNaTela('h1', 'Acertou!');
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Isso aí! você descobriu o número secreto ${numeroSecreto}, 
+        com ${tentativas} ${palavraTentativas}.`;
+        exibirtextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute ('disabled');
+    }else{
+        chute > numeroSecreto ? exibirtextoNaTela('p', 'O número secreto é menor que ' + chute + '.') 
+        : exibirtextoNaTela('p', 'O número secreto é maior que ' + chute + '.');
+        tentativas++; 
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+function limparCampo() {
+    chute= document.querySelector('input');
+    chute.value = '';
+}
+function reiniciarJogo() {
+    numeroSecreto= gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute ('disabled', true);
+}
